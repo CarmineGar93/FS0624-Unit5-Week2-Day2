40293254ced1bb1ee05ed7e6dd82078aa71fad12
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-29 14:16:52 +0100

    Initial commit

---
+++ src/test/java/CarmineGargiulo/FS0624_Unit5_Week2_Day2/Fs0624Unit5Week2Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.FS0624_Unit5_Week2_Day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs0624Unit5Week2Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
